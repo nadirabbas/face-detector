6e9eb5f4b59dbeebc8713280c6f8dd50952ecdb2
Nadir Abbas <nadirabbasforwork@gmail.com>
2022-05-28 21:42:41 +0500

    pretty up

--- scripts/main.js
+++ scripts/main.js
@@ -1,69 +1,106 @@
-// urls
-const models = chrome.extension.getURL("/models");
-const wasm = chrome.extension.getURL("/wasm/");
+(async () => {
+  /* --------------- Initialize extension --------------- */
+  const models = chrome.extension.getURL("/models");
+  const wasm = chrome.extension.getURL("/wasm/");
+  Promise.all([
+    faceapi.nets.ssdMobilenetv1.loadFromUri(models),
+    faceapi.nets.tinyFaceDetector.loadFromUri(models),
+    faceapi.nets.ageGenderNet.loadFromUri(models),
+  ]).then(setToggleHotkey);
 
-// load models on init
-Promise.all([
-  faceapi.nets.ssdMobilenetv1.loadFromUri(models),
-  faceapi.nets.tinyFaceDetector.loadFromUri(models),
-  faceapi.nets.ageGenderNet.loadFromUri(models),
-])
+  // Use WASM resources
+  faceapi.tf.setWasmPaths(wasm);
+  await faceapi.tf.setBackend("wasm");
 
-window.addEventListener('keydown', ({key, altKey}) => {
-  if(key === '`' && altKey) {
-    main();
+  /* --------------- Extension state --------------- */
+  const initialState = {
+    input: null,
+    isRunning: null,
+    animFrame: null,
+  };
+  const state = { ...initialState };
+
+  /* --------------- Remove blocks --------------- */
+  const removeBlocks = () => {
+    document
+      .querySelectorAll(".face-detector-overlay")
+      .forEach((elem) => elem.remove());
+  };
+
+  /* --------------- Reset state --------------- */
+  const reset = () => {
+    Object.assign(state, { ...initialState });
+
+    // remove blocks
+    removeBlocks();
+  };
+
+  /* --------------- Set input --------------- */
+  const setInput = (input) => {
+    state.input = input;
+  };
+
+  /* --------------- Toggle Hotkey --------------- */
+  function setToggleHotkey() {
+    window.addEventListener("keydown", ({ key, altKey }) => {
+      if (key === "`" && altKey) {
+        if (!state.isRunning) {
+          main();
+          state.isRunning = true;
+        } else if (animFrame) {
+          cancelAnimationFrame(animFrame);
+          reset();
+        }
+      }
+    });
   }
-})
 
-// models loaded
-async function main() {
+  /* --------------- The MAIN function --------------- */
+  async function main() {
+    // Set input
+    if (!state.input) setInput(document.querySelector("video"));
+    if (!state.input) return (animFrame = requestAnimationFrame(main));
 
-  faceapi.tf.setWasmPaths(wasm)
-  await faceapi.tf.setBackend('wasm');
+    // Detect faces
+    const detections = await faceapi
+      .detectAllFaces(state.input, new faceapi.TinyFaceDetectorOptions())
+      .withAgeAndGender();
 
-  // video input
-  const input = document.querySelector("video");
-  if (!input) return;
+    // Draw over detected female face(s)
+    drawOvervideo(state.input, detections);
 
-  const detections = await faceapi.detectAllFaces(input, new faceapi.TinyFaceDetectorOptions()).withAgeAndGender()
-  
-  drawOvervideo(input, detections)
-
-  const animFrame = requestAnimationFrame(main);
-
-  const keydown = ({key, altKey}) => {
-    if(key === 's' && altKey) {
-      cancelAnimationFrame(animFrame)
-      window.removeEventListener('keydown', keydown)
-    }
+    // Repeat toggled off
+    animFrame = requestAnimationFrame(main);
   }
 
-  window.addEventListener('keydown', keydown)
-}
+  function drawOvervideo(input, detections) {
+    // Remove garbage blocks
+    removeBlocks();
 
-function drawOvervideo(input, detections) {
+    // Get input bounding rect for block positioning
+    const { left: iL, top: iT } = input.getBoundingClientRect();
 
-  document.querySelectorAll('.face-detector-overlay').forEach(elem => elem.remove())
+    detections.forEach(({ detection, gender, age }) => {
+      if (gender !== "female" || age > 40) return;
 
-  const {left: iL, top: iT} = input.getBoundingClientRect(); 
-  detections.forEach(({detection, gender, age}) => {
-    if(gender === "female" && age < 40) {
-      const {left, top, height, width} = detection.box
+      // Get the detection box coords
+      const { left, top, height, width } = detection.box;
 
-      const div = document.createElement('div')
+      const div = document.createElement("div");
+      div.classList.add("face-detector-overlay");
 
-      div.classList.add('face-detector-overlay')
+      // Style the block
+      div.style.position = "absolute";
+      div.style.left = left + iL + "px";
+      div.style.top = top + iT + "px";
+      div.style.width = width + "px";
+      div.style.height = height + "px";
+      div.style.backgroundColor = "rgba(255, 255, 255, 0)";
+      div.style.backdropFilter = "blur(30px)";
+      div.style.borderRadius = "100%";
 
-      div.style.position = 'absolute';
-      div.style.left = (left + iL) + 'px'
-      div.style.top = (top + iT) + 'px'
-      div.style.width = width + 'px'
-      div.style.height = height + 'px'
-      div.style.backgroundColor = 'rgba(255, 255, 255, 0)'
-      div.style.backdropFilter = 'blur(30px)'
-      div.style.borderRadius = '100%';
-
-      document.body.appendChild(div)
-    }
-  })
-}
+      // Add the block to body
+      document.body.appendChild(div);
+    });
+  }
+})();
